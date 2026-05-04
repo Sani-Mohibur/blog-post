9a390b438667b226420591a87a8c45a589855d5d
Sani-Mohibur <mohiburrahmansani@gmail.com>
2026-05-05 02:29:50 +0600

    feat: comment out loading simulation in about page

--- src/app/(commonLayout)/about/page.tsx
+++ src/app/(commonLayout)/about/page.tsx
@@ -2,7 +2,7 @@ export const dynamic = "force-dynamic";
 
 export default async function AboutPage() {
   // For simulating load time
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  // await new Promise((resolve) => setTimeout(resolve, 5000));
 
   // For simulating error
   // throw new Error("Something went wrong");
