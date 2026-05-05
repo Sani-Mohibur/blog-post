3d630bba86ecf0d51f1f41c21e857b7abe8379c2
Sani-Mohibur <mohiburrahmansani@gmail.com>
2026-05-06 00:01:08 +0600

    feat: implement getBlogs action and update AboutPage to fetch blog data

--- src/app/(commonLayout)/about/page.tsx
+++ src/app/(commonLayout)/about/page.tsx
@@ -1,11 +1,22 @@
-export const dynamic = "force-dynamic";
+"use client";
 
-export default async function AboutPage() {
-  // For simulating load time
-  // await new Promise((resolve) => setTimeout(resolve, 5000));
+import { getBlogs } from "@/actions/blog.action";
+import { useEffect, useState } from "react";
 
-  // For simulating error
-  // throw new Error("Something went wrong");
+export default function AboutPage() {
+  const [data, setData] = useState();
+  const [error, setError] = useState<{ message: string } | null>(null);
+  console.log(data);
+  console.log(error);
+
+  useEffect(() => {
+    (async () => {
+      const { data, error } = await getBlogs();
+
+      setData(data);
+      setError(error);
+    })();
+  }, []);
 
   return (
     <div>
@@ -13,3 +24,9 @@ export default async function AboutPage() {
     </div>
   );
 }
+
+// For simulating load time
+// await new Promise((resolve) => setTimeout(resolve, 5000));
+
+// For simulating error
+// throw new Error("Something went wrong");
